096db9c695fd21d996a5610fb4f02c73dba609d3
Jakub Jakubec <thekobu@gmail.com>
2023-01-28 19:25:30 +0100

    nvim: introduce nvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,6 @@
+require('plugins')
+require "user.options"
+
+--vim.opt.foldmethod = "expr"
+--vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
