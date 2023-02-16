56da9c88546ef577868d0fbf90a42a60fccf824c
Jakub Jakubec <thekobu@gmail.com>
2023-02-16 19:25:30 +0100

    nvim: extract options into separate file

---
+++ .config/nvim/lua/user/options.lua
@@ -0,0 +1,15 @@
+vim.opt.tabstop = 2 
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.relativenumber = true
+vim.opt.nu = true
+vim.opt.backup = false
+vim.opt.hlsearch = false
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.incsearch = true
+vim.opt.scrolloff = 8
+vim.opt.termguicolors = true
+-- vim.opt.colorcolumn = 90
