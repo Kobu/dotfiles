ed38ba2763d56e86f2bdfe8ec4ab55c34b983cd3
Jakub Jakubec <thekobu@gmail.com>
2023-02-18 19:25:30 +0100

    nvim: split LSP settings into multiple files

--- .config/nvim/lua/user/lsp/init.lua
+++ .config/nvim/lua/user/lsp/init.lua
@@ -1,3 +1,37 @@
-require("lspconfig")
-require("user.lsp.lsp-installer")
-require("user.lsp.handlers").setup()
+local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+if not status_ok then
+	return
+end
+
+local lspconfig = require("lspconfig")
+
+local servers = {
+  lua = "sumneko_lua",
+  python = "pyright",
+}
+
+lsp_installer.setup({
+  ensure_installed = servers
+})
+
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+local on_attach = function()
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+  vim.keymap.set("n", "gr", vim.lsp.buf.rename, {buffer=0})
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
+ end
+
+for lang, server in pairs(servers) do
+	local opts = {
+		on_attach = on_attach,
+		capabilities = capabilities,
+	}
+
+	local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. lang)
+	if has_custom_opts then
+		opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
+	end
+
+	lspconfig[server].setup(opts)
+end
