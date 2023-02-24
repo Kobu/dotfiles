3956d6ae006881f431a4cf40a09f08a68f9c674d
Jakub Jakubec <thekobu@gmail.com>
2023-02-24 19:25:30 +0100

    nvim: set space as leader key

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -2,7 +2,8 @@ local opts = {noremap = true, silent = true}
 
 local keymap = vim.api.nvim_set_keymap
 
-keymap("n", "<Space>", "<Nop>", opts)
+-- set <Space> as leader key
+vim.keymap.set("n", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
