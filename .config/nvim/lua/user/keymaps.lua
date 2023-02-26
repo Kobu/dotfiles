8b3c45b47f0dffe66d906bedf2fced951444de48
Jakub Jakubec <thekobu@gmail.com>
2023-02-26 19:25:30 +0100

    nvim: add keybind to comment code

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -14,4 +14,14 @@ keymap("v", ">", ">gv", opts)
 
 keymap("v", "<A-j>", ":m .+1<CR>==", opts)
 keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+-- comment out current line a go to next line
+vim.keymap.set("n", "<C-_>",
+  function()
+    require("Comment.api").toggle_current_linewise()
+    vim.cmd('+1')
+  end
+)
+-- comment out highlighted block
+vim.keymap.set("v", "<C-_>",
+  "<ESC><cmd>lua require('Comment.api').toggle_linewise_op(vim.fn.visualmode())<CR>")
 
