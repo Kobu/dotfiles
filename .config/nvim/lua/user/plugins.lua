c8931a4600f72536754055fcaf7ea811515d9acb
Jakub Jakubec <thekobu@gmail.com>
2023-03-03 19:25:30 +0100

    nvim: add comment plugin

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -22,20 +22,24 @@ packer.init {
   },
 }
 
+
 return packer.startup(function(use)
   use "wbthomason/packer.nvim"
+
   use {
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate"
          }
   use "neovim/nvim-lspconfig"
   use "williamboman/nvim-lsp-installer"
+
   use "hrsh7th/nvim-cmp"
   use "hrsh7th/cmp-nvim-lsp"
   use "hrsh7th/cmp-buffer"
   use "hrsh7th/cmp-path"
+
   use "folke/tokyonight.nvim"
-  use "L3MON4D3/LuaSnip"
+
   use "windwp/nvim-autopairs"
 
   use "numToStr/Comment.nvim"
