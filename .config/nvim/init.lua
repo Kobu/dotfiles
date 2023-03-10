aa93ce8eea4e8abc07f8ec4b39fa7b4261a4ff53
Jakub Jakubec <thekobu@gmail.com>
2023-03-10 19:25:30 +0100

    nvim: include treesitter in init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,15 +4,5 @@ require "user.plugins"
 require "user.colorscheme"
 require "user.cmp"
 require "user.autopairs"
-local configs = require'nvim-treesitter.configs'
-
-configs.setup {
-ensure_installed = {"c", "lua", "python", "javascript"} , -- Only use parsers that are maintained
-highlight = { -- enable highlighting
-  enable = true,
-},
-indent = {
-  enable = true,
-}
-}
 require "user.lsp"
+require "user.treesitter"
