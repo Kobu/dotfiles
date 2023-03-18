b6e3c4ab6c0033186b4447b0598458f7d5ce2947
Jakub Jakubec <thekobu@gmail.com>
2023-03-18 19:25:30 +0100

    polybar/awesome: stylize polybar and awesome

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -93,7 +93,7 @@ end)
 
 screen.connect_signal("request::desktop_decoration", function(s)
     -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5"}, s, awful.layout.layouts[1])
+    awful.tag({ "work", "docs", "result", "maintanence", "offtopic"}, s, awful.layout.layouts[1])
 end)
 
 -- }}}
