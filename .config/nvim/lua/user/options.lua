7fcf539bd688b4fa1d41ab7346732ca543064a3f
Jakub Jakubec <thekobu@gmail.com>
2023-04-16 20:25:31 +0200

    nvim: update whitespace related options

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -1,6 +1,6 @@
-vim.opt.tabstop = 2 
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.relativenumber = true
