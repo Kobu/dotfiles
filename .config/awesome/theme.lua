3bc0ad198dd8b70182ff5ee4db7e33b2127c3bd9
Jakub Jakubec <thekobu@gmail.com>
2023-05-18 20:25:31 +0200

    awesome: add stylua formatting

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -13,25 +13,25 @@ local gfs = require("gears.filesystem")
 local themes_path = gfs.get_themes_dir()
 
 -- inherit default theme
-local theme = dofile(themes_path.."default/theme.lua")
+local theme = dofile(themes_path .. "default/theme.lua")
 -- load vector assets' generators for this theme
 
-theme.font          = "sans 8"
+theme.font = "sans 8"
 
-theme.bg_normal     = xrdb.background
-theme.bg_focus      = xrdb.color12
-theme.bg_urgent     = xrdb.color9
-theme.bg_minimize   = xrdb.color8
-theme.bg_systray    = theme.bg_normal
+theme.bg_normal = xrdb.background
+theme.bg_focus = xrdb.color12
+theme.bg_urgent = xrdb.color9
+theme.bg_minimize = xrdb.color8
+theme.bg_systray = theme.bg_normal
 
-theme.fg_normal     = xrdb.foreground
-theme.fg_focus      = theme.bg_normal
-theme.fg_urgent     = theme.bg_normal
-theme.fg_minimize   = theme.bg_normal
+theme.fg_normal = xrdb.foreground
+theme.fg_focus = theme.bg_normal
+theme.fg_urgent = theme.bg_normal
+theme.fg_minimize = theme.bg_normal
 
-theme.useless_gap   = dpi(6)
-theme.border_width  = dpi(2)
-theme.border_color_normal ="#00FFFF"
+theme.useless_gap = dpi(6)
+theme.border_width = dpi(2)
+theme.border_color_normal = "#00FFFF"
 theme.border_color_active = "#cb65d6" --theme.bg_focus
 theme.border_color_marked = xrdb.color10
 
@@ -51,9 +51,9 @@ theme.tooltip_bg = theme.bg_normal
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = themes_path.."default/submenu.png"
+theme.menu_submenu_icon = themes_path .. "default/submenu.png"
 theme.menu_height = dpi(16)
-theme.menu_width  = dpi(100)
+theme.menu_width = dpi(100)
 
 -- You can add as many variables as
 -- you wish and access them by using
@@ -66,56 +66,42 @@ theme = theme_assets.recolor_layout(theme, theme.fg_normal)
 -- Recolor titlebar icons:
 --
 local function darker(color_value, darker_n)
-    local result = "#"
-    for s in color_value:gmatch("[a-fA-F0-9][a-fA-F0-9]") do
-        local bg_numeric_value = tonumber("0x"..s) - darker_n
-        if bg_numeric_value < 0 then bg_numeric_value = 0 end
-        if bg_numeric_value > 255 then bg_numeric_value = 255 end
-        result = result .. string.format("%2.2x", bg_numeric_value)
+  local result = "#"
+  for s in color_value:gmatch("[a-fA-F0-9][a-fA-F0-9]") do
+    local bg_numeric_value = tonumber("0x" .. s) - darker_n
+    if bg_numeric_value < 0 then
+      bg_numeric_value = 0
     end
-    return result
+    if bg_numeric_value > 255 then
+      bg_numeric_value = 255
+    end
+    result = result .. string.format("%2.2x", bg_numeric_value)
+  end
+  return result
 end
-theme = theme_assets.recolor_titlebar(
-    theme, theme.fg_normal, "normal"
-)
-theme = theme_assets.recolor_titlebar(
-    theme, darker(theme.fg_normal, -60), "normal", "hover"
-)
-theme = theme_assets.recolor_titlebar(
-    theme, xrdb.color1, "normal", "press"
-)
-theme = theme_assets.recolor_titlebar(
-    theme, theme.fg_focus, "focus"
-)
-theme = theme_assets.recolor_titlebar(
-    theme, darker(theme.fg_focus, -60), "focus", "hover"
-)
-theme = theme_assets.recolor_titlebar(
-    theme, xrdb.color1, "focus", "press"
-)
+theme = theme_assets.recolor_titlebar(theme, theme.fg_normal, "normal")
+theme = theme_assets.recolor_titlebar(theme, darker(theme.fg_normal, -60), "normal", "hover")
+theme = theme_assets.recolor_titlebar(theme, xrdb.color1, "normal", "press")
+theme = theme_assets.recolor_titlebar(theme, theme.fg_focus, "focus")
+theme = theme_assets.recolor_titlebar(theme, darker(theme.fg_focus, -60), "focus", "hover")
+theme = theme_assets.recolor_titlebar(theme, xrdb.color1, "focus", "press")
 
 -- Define the icon theme for application icons. If not set then the icons
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
 theme.icon_theme = nil
 
 -- Generate Awesome icon:
-theme.awesome_icon = theme_assets.awesome_icon(
-    theme.menu_height, theme.bg_focus, theme.fg_focus
-)
+theme.awesome_icon = theme_assets.awesome_icon(theme.menu_height, theme.bg_focus, theme.fg_focus)
 
 -- Generate taglist squares:
 local taglist_square_size = dpi(4)
-theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
-    taglist_square_size, theme.fg_normal
-)
-theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
-    taglist_square_size, theme.fg_normal
-)
+theme.taglist_squares_sel = theme_assets.taglist_squares_sel(taglist_square_size, theme.fg_normal)
+theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(taglist_square_size, theme.fg_normal)
 
 -- Try to determine if we are running light or dark colorscheme:
-local bg_numberic_value = 0;
+local bg_numberic_value = 0
 for s in theme.bg_normal:gmatch("[a-fA-F0-9][a-fA-F0-9]") do
-    bg_numberic_value = bg_numberic_value + tonumber("0x"..s);
+  bg_numberic_value = bg_numberic_value + tonumber("0x" .. s)
 end
 local is_dark_bg = (bg_numberic_value < 383)
 
@@ -124,18 +110,18 @@ local wallpaper_bg = xrdb.color8
 local wallpaper_fg = xrdb.color7
 local wallpaper_alt_fg = xrdb.color12
 if not is_dark_bg then
-    wallpaper_bg, wallpaper_fg = wallpaper_fg, wallpaper_bg
+  wallpaper_bg, wallpaper_fg = wallpaper_fg, wallpaper_bg
 end
 
 theme.wallpaper = "/home/kobu/.config/awesome/wallpapers/good.png"
 theme.wallpaper_bg = wallpaper_bg
 
 -- Set different colors for urgent notifications.
-rnotification.connect_signal('request::rules', function()
-    rnotification.append_rule {
-        rule       = { urgency = 'critical' },
-        properties = { bg = '#ff0000', fg = '#ffffff' }
-    }
+rnotification.connect_signal("request::rules", function()
+  rnotification.append_rule({
+    rule = { urgency = "critical" },
+    properties = { bg = "#ff0000", fg = "#ffffff" },
+  })
 end)
 
 return theme
