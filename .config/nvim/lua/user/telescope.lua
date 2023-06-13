0729248bd60bd1bbe56602d0f0980b9008a3aac4
Kobu <thekobu@gmail.com>
2023-06-13 20:25:32 +0200

    nvim: prevent telescope from failing when not in a git repo

--- .config/nvim/lua/user/telescope.lua
+++ .config/nvim/lua/user/telescope.lua
@@ -1,34 +1,39 @@
 local telescope = load_plugin("telescope")
 if not telescope then
-    return
+  return
 end
 
 local builtin = load_plugin("telescope.builtin")
 if not builtin then
-    return
+  return
 end
 
 local actions = load_plugin("telescope.actions")
 if not actions then
-    return
+  return
 end
-
 
 vim.keymap.set("n", "<leader>aa", builtin.find_files, {})
 vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>ht", builtin.help_tags, {})
-vim.keymap.set("n", "<leader>ff", builtin.git_files, {})
+vim.keymap.set("n", "<leader>ff", function()
+  vim.fn.system("git rev-parse --is-inside-work-tree 2> /dev/null")
+  if vim.v.shell_error == 0 then
+    builtin.git_files()
+  else
+    builtin.find_files()
+  end
+end, {})
 vim.keymap.set("n", "<leader>r", builtin.lsp_references, {})
 vim.keymap.set("n", "e", builtin.resume, {})
 
-
 telescope.setup({
-    defaults = {
-        mappings = {
-            i = {
-                ["<C-j>"] = actions.move_selection_next,
-                ["<C-k>"] = actions.move_selection_previous,
-            },
-        },
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+      },
     },
+  },
 })
