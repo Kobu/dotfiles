a62e27d11db6f43c77a35a7fc867a85afb800626
Kobu <thekobu@gmail.com>
2023-06-27 20:25:32 +0200

    nvim: delete unused : symbol in todo-comment regex

--- .config/nvim/lua/user/todo-comments.lua
+++ .config/nvim/lua/user/todo-comments.lua
@@ -63,7 +63,7 @@ todo_comments.setup({
     },
     -- regex that will be used to match keywords.
     -- don't replace the (KEYWORDS) placeholder
-    pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+    pattern = [[\b(KEYWORDS)]], -- ripgrep regex
     -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
   },
 })
