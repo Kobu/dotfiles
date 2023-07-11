72bdb41a2f9111439da65d58fc1f5fae1faf093d
Kobu <thekobu@gmail.com>
2023-07-11 23:46:40 +0200

    awesome: finalize theme.rc config cleanup

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -1,44 +1,25 @@
----------------------------------------------
--- Awesome theme which follows xrdb config --
---   by Yauhen Kirylau                    --
----------------------------------------------
-
 local theme_assets = require("beautiful.theme_assets")
 local xresources = require("beautiful.xresources")
 local rnotification = require("ruled.notification")
 local dpi = xresources.apply_dpi
-local xrdb = xresources.get_current_theme()
-local gdebug = require("gears.debug")
-local gfs = require("gears.filesystem")
-local themes_path = gfs.get_themes_dir()
 
--- inherit default theme
-local theme = dofile(themes_path .. "default/theme.lua")
--- load vector assets' generators for this theme
+local theme = {}
+theme.wallpaper = "/home/kobu/.config/awesome/wallpapers/nord.png"
 
-theme.font = "sans 8"
-
+-- Styling
 theme.useless_gap = dpi(6)
 theme.border_width = dpi(2)
+
+-- Colors
 theme.border_color_normal = "#011826"
 theme.border_color_active = "#D97E96"
 
-
--- Define the icon theme for application icons. If not set then the icons
--- from /usr/share/icons and /usr/share/icons/hicolor will be used.
-theme.icon_theme = nil
-
--- Generate Awesome icon:
-theme.awesome_icon = theme_assets.awesome_icon(theme.menu_height, theme.bg_focus, theme.fg_focus)
-
--- Generate taglist squares:
+-- Generate taglist squares
 local taglist_square_size = dpi(4)
 theme.taglist_squares_sel = theme_assets.taglist_squares_sel(taglist_square_size, theme.fg_normal)
 theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(taglist_square_size, theme.fg_normal)
 
-theme.wallpaper = "/home/kobu/.config/awesome/wallpapers/nord.png"
-
--- Set different colors for urgent notifications.
+-- Notifications
 rnotification.connect_signal("request::rules", function()
   rnotification.append_rule({
     rule = { urgency = "critical" },
