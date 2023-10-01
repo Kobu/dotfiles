e1c93a03947434bb8dabc61fe6fe26c02bbafcf3
Kobu <thekobu@gmail.com>
2023-10-01 15:34:33 +0200

    nvim: overwrite default action for git commits telescope picker

--- .config/nvim/lua/user/telescope.lua
+++ .config/nvim/lua/user/telescope.lua
@@ -13,6 +13,11 @@ if not actions then
   return
 end
 
+local action_state = load_plugin("telescope.actions.state")
+if not action_state then
+  return
+end
+
 vim.keymap.set("n", "<leader>aa", builtin.find_files, {})
 vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>ht", builtin.help_tags, {})
@@ -36,6 +41,17 @@ end, {})
 vim.keymap.set("n", "e", builtin.resume, {})
 vim.keymap.set("n", "<leader>td", ":TodoTelescope<CR>", {})
 
+local function getSha()
+  local selection = action_state.get_selected_entry()
+  local sha = selection.value
+  vim.fn.system("echo " .. sha .. " | xclip -selection clipboard")
+  if vim.v.shell_error ~= 0 then
+    vim.notify("Error copying the SHA", "error")
+    return
+  end
+  vim.notify("Copied " .. sha .. " to clipboard")
+end
+
 telescope.setup({
   defaults = {
     mappings = {
@@ -43,6 +59,21 @@ telescope.setup({
         ["<C-j>"] = actions.move_selection_next,
         ["<C-k>"] = actions.move_selection_previous,
       },
+    },
+  },
+  pickers = {
+    git_bcommits = {
+      mappings = {
+        i = {
+          ["<cr>"] = actions.select_vertical,
+        },
+      },
+    },
+    git_commits = {
+      attach_mappings = function(_, map)
+        map("i", "<cr>", getSha)
+        return true
+      end,
     },
   },
 })
