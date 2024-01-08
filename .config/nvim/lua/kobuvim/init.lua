9f6c5e4a81716b3cab70094b1fc2a2a9b19bddca
Kobu <thekobu@gmail.com>
2024-01-09 00:33:54 +0100

    nvim: make notifications appear in bottom-right corner

--- .config/nvim/lua/kobuvim/init.lua
+++ .config/nvim/lua/kobuvim/init.lua
@@ -3,7 +3,12 @@ local utils = require("kobuvim.utils")
 local user_mappings = require("user.mappings")
 local mappings_helper = require("kobuvim.mappings")
 
+local notify = require("notify")
+notify.setup({
+    top_down = false
+})
+
 -- set notify.nvim as default notification function
-vim.notify = require("notify")
+vim.notify = notify
 
 mappings_helper.set_mappings(user_mappings)
