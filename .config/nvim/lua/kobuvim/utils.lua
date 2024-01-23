fc7868b5a53653070ec25f70a14fc7561a70bb80
thekobu@gmail.com <kobu@NB-01-JAKUBJ.>
2024-01-23 21:29:02 +0100

    nvim: add utilities to work with os commands

--- .config/nvim/lua/kobuvim/utils.lua
+++ .config/nvim/lua/kobuvim/utils.lua
@@ -11,17 +11,65 @@ end
 -- keymap: string (new keymap to be used instead)
 -- command: function (the original function ran on this keymap)
 function outdated_keymap(opts)
-  return function()
-    vim.notify(
-      {
-        opts.message,
-        opts.keymap and ("Use '" .. opts.keymap .. "' instead!") or nil,
-      },
-      vim.log.levels.WARN,
-      {
-        title = "This keymap is deprecated!!",
-      }
-    )
-    opts.command()
-  end
+    return function()
+        vim.notify(
+            {
+                opts.message,
+                opts.keymap and ("Use '" .. opts.keymap .. "' instead!") or nil,
+            },
+            vim.log.levels.WARN,
+            {
+                title = "This keymap is deprecated!!",
+            }
+        )
+        opts.command()
+    end
+end
+
+function vim.getVisualSelection()
+    vim.cmd('noau normal! "vy"')
+    local text = vim.fn.getreg('v')
+    vim.fn.setreg('v', {})
+
+    text = string.gsub(text, "\n", "")
+    if #text > 0 then
+        return text
+    else
+        return ''
+    end
+end
+
+local function parse_command_output(output)
+    local list = {}
+
+    -- check if output is a string
+    if type(output) ~= 'string' then
+        vim.notify('command did not return a string')
+        return
+    end
+
+    for token in string.gmatch(output, "[^%c]+") do
+        table.insert(list, token)
+    end
+
+    return list
+end
+
+local function execute_command(command)
+    local handle = io.popen(command)
+
+    if (handle == nil) then
+        print('could not run specified command:' .. command)
+        return
+    end
+
+    local output = handle:read("*a")
+
+    handle:close()
+
+    return output
+end
+
+function vim.run_command(command)
+    return parse_command_output(execute_command(command))
 end
