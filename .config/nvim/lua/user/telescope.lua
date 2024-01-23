4594a86905db37a94b3c4bd7c67a57261a06ff2e
thekobu@gmail.com <kobu@NB-01-JAKUBJ.>
2024-01-23 21:40:00 +0100

    nvim: add option to specify folder in live_grep

--- .config/nvim/lua/user/telescope.lua
+++ .config/nvim/lua/user/telescope.lua
@@ -1,101 +1,220 @@
+local scan = require 'plenary.scandir'
+local transform_mod = require('telescope.actions.mt').transform_mod
+
+local action_set = require 'telescope.actions.set'
+local make_entry = require 'telescope.make_entry'
+
+local conf = require("telescope.config").values
+
 local telescope = load_plugin("telescope")
 if not telescope then
-  return
+    return
+end
+local previewers = load_plugin("telescope.previewers")
+if not previewers then
+    return
+end
+
+
+
+local finders = load_plugin("telescope.finders")
+if not finders then
+    return
+end
+
+
+local pickers = load_plugin("telescope.pickers")
+if not pickers then
+    return
 end
 
 local builtin = load_plugin("telescope.builtin")
 if not builtin then
-  return
+    return
 end
 
 local actions = load_plugin("telescope.actions")
 if not actions then
-  return
+    return
 end
 
 local action_state = load_plugin("telescope.actions.state")
 if not action_state then
-  return
+    return
 end
 
 local hot_keys = load_plugin("user.hot-keys")
 if not hot_keys then
-  return
+    return
 end
+local live_grep_filters = {
+    ---@type nil|string
+    extension = nil,
+    ---@type nil|string[]
+    directories = nil,
+}
 
 vim.keymap.set("n", "<leader>aa", builtin.find_files, {})
 vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>ht", builtin.help_tags, {})
 vim.keymap.set("n", "<leader>km", builtin.keymaps, {})
-vim.keymap.set("n", "<leader>hk", function ()
-    hot_keys.picker(require("telescope.themes").get_dropdown{})
+vim.keymap.set("n", "<leader>hk", function()
+    hot_keys.picker(require("telescope.themes").get_dropdown {})
 end, {})
 vim.keymap.set("n", "gs", builtin.git_status, {})
 vim.keymap.set("n", "gb", builtin.git_branches, {})
 vim.keymap.set("n", "si", builtin.lsp_implementations, {})
 vim.keymap.set("n", "sd", builtin.lsp_definitions, { buffer = 0 })
 vim.keymap.set("n", "gl", function()
-  builtin.git_commits({
-    git_command = { "git", "log", "--format=%C(bold blue)%h %C(211)[Date: %as] %C(magenta)[%an] %Creset%s" },
+    builtin.git_commits({
+        git_command = { "git", "log", "--format=%C(bold blue)%h %C(211)[Date: %as] %C(magenta)[%an] %Creset%s" },
 
-  })
+    })
 end, {})
 vim.keymap.set("n", "<leader>ff", function()
-  vim.fn.system("git rev-parse --is-inside-work-tree 2> /dev/null")
-  if vim.v.shell_error == 0 then
-    builtin.git_files()
-  else
-    builtin.find_files()
-  end
+    vim.fn.system("git rev-parse --is-inside-work-tree 2> /dev/null")
+    if vim.v.shell_error == 0 then
+        builtin.git_files()
+    else
+        builtin.find_files()
+    end
 end, {})
 vim.keymap.set("n", "sr", function()
-  builtin.lsp_references({ jump_type = "never" })
+    builtin.lsp_references({ jump_type = "never" })
 end, {})
 vim.keymap.set("n", "e", builtin.resume, {})
 vim.keymap.set("n", "<leader>td", ":TodoTelescope<CR>", {})
 
 local function getSha()
-  local selection = action_state.get_selected_entry()
-  local sha = selection.value
-  vim.fn.system("echo " .. sha .. " | xclip -selection clipboard")
-  if vim.v.shell_error ~= 0 then
-    vim.notify("Error copying the SHA", "error")
-    return
-  end
-  vim.notify("Copied " .. sha .. " to clipboard")
+    local selection = action_state.get_selected_entry()
+    local sha = selection.value
+    vim.fn.system("echo " .. sha .. " | xclip -selection clipboard")
+    if vim.v.shell_error ~= 0 then
+        vim.notify("Error copying the SHA", "error")
+        return
+    end
+    vim.notify("Copied " .. sha .. " to clipboard")
 end
 
 local function doRebase(bufnr)
-  local selection = action_state.get_selected_entry()
-  local sha = selection.value
-  actions.smart_send_to_qflist(bufnr)
-  vim.cmd("Git rebase -i " .. sha .. "^")
+    local selection = action_state.get_selected_entry()
+    local sha = selection.value
+    actions.smart_send_to_qflist(bufnr)
+    vim.cmd("Git rebase -i " .. sha .. "^")
 end
 
+---Run `live_grep` with the active filters (extension and folders)
+local function run_live_grep(current_input)
+  -- TODO: Resume old one with same options somehow
+  require('telescope.builtin').live_grep {
+    additional_args = live_grep_filters.extension and function()
+      return { '-g', '*.' .. live_grep_filters.extension }
+    end,
+    search_dirs = live_grep_filters.directories,
+    default_text = current_input,
+  }
+end
+
+local os_sep = "/"
+
+local my_actions = transform_mod {
+  ---Ask for a file extension and open a new `live_grep` filtering by it
+  set_extension = function(prompt_bufnr)
+    local current_picker = action_state.get_current_picker(prompt_bufnr)
+    local current_input = action_state.get_current_line()
+
+    vim.ui.input({ prompt = '*.' }, function(input)
+      if input == nil then
+        return
+      end
+
+      live_grep_filters.extension = input
+
+      actions._close(prompt_bufnr, current_picker.initial_mode == 'insert')
+      run_live_grep(current_input)
+    end)
+  end,
+  ---Ask the user for a folder and olen a new `live_grep` filtering by it
+  set_folders = function(prompt_bufnr)
+    local current_picker = action_state.get_current_picker(prompt_bufnr)
+    local current_input = action_state.get_current_line()
+
+    local data = {}
+    scan.scan_dir(vim.loop.cwd(), {
+      hidden = true,
+      only_dirs = true,
+      respect_gitignore = true,
+      on_insert = function(entry)
+        table.insert(data, entry .. os_sep)
+      end,
+    })
+    table.insert(data, 1, '.' .. os_sep)
+
+    actions._close(prompt_bufnr, current_picker.initial_mode == 'insert')
+    pickers.new({}, {
+      prompt_title = 'Folders for Live Grep',
+      finder = finders.new_table { results = data, entry_maker = make_entry.gen_from_file {} },
+      previewer = conf.file_previewer {},
+      sorter = conf.file_sorter {},
+      attach_mappings = function(prompt_bufnr)
+        action_set.select:replace(function()
+          local current_picker = action_state.get_current_picker(prompt_bufnr)
+
+          local dirs = {}
+          local selections = current_picker:get_multi_selection()
+          if vim.tbl_isempty(selections) then
+            table.insert(dirs, action_state.get_selected_entry().value)
+          else
+            for _, selection in ipairs(selections) do
+              table.insert(dirs, selection.value)
+            end
+          end
+          live_grep_filters.directories = dirs
+
+          actions.close(prompt_bufnr)
+          run_live_grep(current_input)
+        end)
+        return true
+      end,
+    }):find()
+  end,
+}
+
+
+
+
+
 telescope.setup({
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-      },
-    },
-  },
-  pickers = {
-    git_bcommits = {
-      mappings = {
-        i = {
-          ["<cr>"] = actions.select_vertical,
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+            },
         },
-      },
     },
-    git_commits = {
-      mappings = {
-        i = {
-          ["<cr>"] = getSha,
-          ["R"] = doRebase,
+    pickers = {
+        live_grep = {
+            mappings = {
+                i = {
+                ["<C-p>"] = my_actions.set_folders
+                }
+            }
         },
-      },
+        git_bcommits = {
+            mappings = {
+                i = {
+                    ["<cr>"] = actions.select_vertical,
+                },
+            },
+        },
+        git_commits = {
+            mappings = {
+                i = {
+                    ["<cr>"] = getSha,
+                    ["R"] = doRebase,
+                },
+            },
+        },
     },
-  },
 })
