8f7dc3fca5a9f39a0e53e829c8f0e118e411bad4
Kobu <thekobu@gmail.com>
2024-02-03 21:38:36 +0100

    nvim: add eslint_d as diagnostics and code actions

--- .config/nvim/lua/user/null-ls.lua
+++ .config/nvim/lua/user/null-ls.lua
@@ -4,13 +4,23 @@ if not null_ls then
 end
 
 local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
+
 
 null_ls.setup({
   debug = false,
   sources = {
+    -- formatting
     formatting.black.with({ extra_args = { "--fast" } }),
     formatting.xmlformat,
     formatting.stylua,
     formatting.eslint_d,
+
+    -- diagnostics
+    diagnostics.eslint_d,
+
+    -- code_actions
+    code_actions.eslint_d,
   },
 })
