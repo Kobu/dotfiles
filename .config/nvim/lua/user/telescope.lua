ffe403ee71b1958019184547c4f630b00e1ab5b4
thekobu@gmail.com <thekobu@gmail.com>
2024-03-08 14:45:47 +0100

    nvim: add option to rebase or get sha of a commit

--- .config/nvim/lua/user/telescope.lua
+++ .config/nvim/lua/user/telescope.lua
@@ -117,81 +117,82 @@ end
 
 ---Run `live_grep` with the active filters (extension and folders)
 local function run_live_grep(current_input)
-    -- TODO: Resume old one with same options somehow
-    require('telescope.builtin').live_grep {
-        additional_args = live_grep_filters.extension and function()
-            return { '-g', '*.' .. live_grep_filters.extension }
-        end,
-        search_dirs = live_grep_filters.directories,
-        default_text = current_input,
-    }
+  -- TODO: Resume old one with same options somehow
+  require("telescope.builtin").live_grep({
+    additional_args = live_grep_filters.extension and function()
+      return { "-g", "*." .. live_grep_filters.extension }
+    end,
+    search_dirs = live_grep_filters.directories,
+    default_text = current_input,
+  })
 end
 
 local os_sep = "/"
 
-local my_actions = transform_mod {
-    ---Ask for a file extension and open a new `live_grep` filtering by it
-    set_extension = function(prompt_bufnr)
-        local current_picker = action_state.get_current_picker(prompt_bufnr)
-        local current_input = action_state.get_current_line()
+local my_actions = transform_mod({
+  ---Ask for a file extension and open a new `live_grep` filtering by it
+  set_extension = function(prompt_bufnr)
+    local current_picker = action_state.get_current_picker(prompt_bufnr)
+    local current_input = action_state.get_current_line()
 
-        vim.ui.input({ prompt = '*.' }, function(input)
-            if input == nil then
-                return
+    vim.ui.input({ prompt = "*." }, function(input)
+      if input == nil then
+        return
+      end
+
+      live_grep_filters.extension = input
+
+      actions._close(prompt_bufnr, current_picker.initial_mode == "insert")
+      run_live_grep(current_input)
+    end)
+  end,
+  ---Ask the user for a folder and olen a new `live_grep` filtering by it
+  set_folders = function(prompt_bufnr)
+    local current_picker = action_state.get_current_picker(prompt_bufnr)
+    local current_input = action_state.get_current_line()
+
+    local data = {}
+    scan.scan_dir(vim.loop.cwd(), {
+      hidden = true,
+      only_dirs = true,
+      respect_gitignore = true,
+      on_insert = function(entry)
+        table.insert(data, entry .. os_sep)
+      end,
+    })
+    table.insert(data, 1, "." .. os_sep)
+
+    actions._close(prompt_bufnr, current_picker.initial_mode == "insert")
+    pickers
+      .new({}, {
+        prompt_title = "Folders for Live Grep",
+        finder = finders.new_table({ results = data, entry_maker = make_entry.gen_from_file({}) }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.file_sorter({}),
+        attach_mappings = function(prompt_bufnr)
+          action_set.select:replace(function()
+            local current_picker = action_state.get_current_picker(prompt_bufnr)
+
+            local dirs = {}
+            local selections = current_picker:get_multi_selection()
+            if vim.tbl_isempty(selections) then
+              table.insert(dirs, action_state.get_selected_entry().value)
+            else
+              for _, selection in ipairs(selections) do
+                table.insert(dirs, selection.value)
+              end
             end
+            live_grep_filters.directories = dirs
 
-            live_grep_filters.extension = input
-
-            actions._close(prompt_bufnr, current_picker.initial_mode == 'insert')
+            actions.close(prompt_bufnr)
             run_live_grep(current_input)
-        end)
-    end,
-    ---Ask the user for a folder and olen a new `live_grep` filtering by it
-    set_folders = function(prompt_bufnr)
-        local current_picker = action_state.get_current_picker(prompt_bufnr)
-        local current_input = action_state.get_current_line()
-
-        local data = {}
-        scan.scan_dir(vim.loop.cwd(), {
-            hidden = true,
-            only_dirs = true,
-            respect_gitignore = true,
-            on_insert = function(entry)
-                table.insert(data, entry .. os_sep)
-            end,
-        })
-        table.insert(data, 1, '.' .. os_sep)
-
-        actions._close(prompt_bufnr, current_picker.initial_mode == 'insert')
-        pickers.new({}, {
-            prompt_title = 'Folders for Live Grep',
-            finder = finders.new_table { results = data, entry_maker = make_entry.gen_from_file {} },
-            previewer = conf.file_previewer {},
-            sorter = conf.file_sorter {},
-            attach_mappings = function(prompt_bufnr)
-                action_set.select:replace(function()
-                    local current_picker = action_state.get_current_picker(prompt_bufnr)
-
-                    local dirs = {}
-                    local selections = current_picker:get_multi_selection()
-                    if vim.tbl_isempty(selections) then
-                        table.insert(dirs, action_state.get_selected_entry().value)
-                    else
-                        for _, selection in ipairs(selections) do
-                            table.insert(dirs, selection.value)
-                        end
-                    end
-                    live_grep_filters.directories = dirs
-
-                    actions.close(prompt_bufnr)
-                    run_live_grep(current_input)
-                end)
-                return true
-            end,
-        }):find()
-    end,
-}
-
+          end)
+          return true
+        end,
+      })
+      :find()
+  end,
+})
 
 telescope.setup({
   extensions = {
@@ -200,18 +201,18 @@ telescope.setup({
       -- This extension's options, see below.
     },
     defaults = {
-        layout_strategy = "vertical",
-        layout_config = { height = 0.95, width = 0.95, preview_height = 0.7 },
-        path_display = { "truncate" },
-        mappings = {
-            i = {
-                ["<C-j>"] = actions.move_selection_next,
-                ["<C-q>"] = actions.close,
-                ["<C-k>"] = actions.move_selection_previous,
-                ["<C-h>"] = actions.preview_scrolling_left,
-                ["<C-l>"] = actions.preview_scrolling_right,
-            },
+      layout_strategy = "vertical",
+      layout_config = { height = 0.95, width = 0.95, preview_height = 0.7 },
+      path_display = { "truncate" },
+      mappings = {
+        i = {
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-q>"] = actions.close,
+          ["<C-k>"] = actions.move_selection_previous,
+          ["<C-h>"] = actions.preview_scrolling_left,
+          ["<C-l>"] = actions.preview_scrolling_right,
         },
+      },
     },
   },
   pickers = {
@@ -237,5 +238,5 @@ telescope.setup({
         },
       },
     },
-  }
+  },
 })
