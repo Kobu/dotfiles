5029e0e890537ff8ed254ec89872bbc0a9f284e2
Kobu <thekobu@gmail.com>
2025-04-24 17:19:24 +0200

    [nowaster]: add session_getter

---
+++ utils/session_getter.sh
@@ -0,0 +1,28 @@
+TOKEN=$(cat ~/utils/.clerk-token)
+DATA=$(curl -s -X GET \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Authorization: Bearer $TOKEN" \
+  "https://nowaster-web-production.up.railway.app/api/session/active")
+
+if [ "$DATA" = "" ] || [ "$DATA" = "null" ] ; then
+    echo ""
+    exit 1
+fi
+
+format () {
+    date --date=$1 +"%H:%M"
+}
+
+if [ "$(echo $DATA | jq ".data")" = "[]" ]; then
+    echo ""
+    exit 0
+fi
+
+CAT=$(echo $DATA | jq ".data.[0].category.name" | tr -d '"')
+START=$(format $(echo $DATA | jq ".data.[0].startTime" | tr -d '"'))
+END=$(format $(echo $DATA | jq ".data.[0].endTime" | tr -d '"'))
+TAGS=$(echo $DATA | jq "[.data[].tags[].label] | unique | join(\", \") | \"(\" + . + \")\"" | tr -d '[]"')
+let DIFF=($(date +%s -d $END)-$(date +%s))/60
+
+echo "[${CAT}] $TAGS: ${START}->${END} (${DIFF}mins)"
