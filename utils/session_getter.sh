4886663d4bb777697a959a39e0664929170ba87a
Kobu <thekobu@gmail.com>
2025-05-08 14:18:19 +0200

    nowaster: update session getter with active sessions

--- utils/session_getter.sh
+++ utils/session_getter.sh
@@ -3,7 +3,7 @@ DATA=$(curl -s -X GET \
   -H "Content-Type: application/json" \
   -H "Accept: application/json" \
   -H "Authorization: Bearer $TOKEN" \
-  "https://nowaster-web-production.up.railway.app/api/session/active")
+  "https://nowaster-web-production.up.railway.app/api/session/fixed/active")
 
 if [ "$DATA" = "" ] || [ "$DATA" = "null" ] ; then
     echo ""
@@ -23,6 +23,15 @@ CAT=$(echo $DATA | jq ".data.[0].category.name" | tr -d '"')
 START=$(format $(echo $DATA | jq ".data.[0].startTime" | tr -d '"'))
 END=$(format $(echo $DATA | jq ".data.[0].endTime" | tr -d '"'))
 TAGS=$(echo $DATA | jq "[.data[].tags[].label] | unique | join(\", \") | \"(\" + . + \")\"" | tr -d '[]"')
-let DIFF=($(date +%s -d $END)-$(date +%s))/60
 
+# if  end is null then show start time and how many minutes ago it was
+if [ "$END" = "" ]; then
+    # set end as now
+    END=$(date +"%H:%M")
+    let DIFF=($(date +%s -d $END)-$(date +%s -d $START))/60
+    echo "[${CAT}] $TAGS: ${START}->now (${DIFF}mins)"
+    exit 0
+fi
+
+let DIFF=($(date +%s -d $END)-$(date +%s))/60
 echo "[${CAT}] $TAGS: ${START}->${END} (${DIFF}mins)"
